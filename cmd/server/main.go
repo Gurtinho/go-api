fc67cc3e79f74db2b1300a46e0197713a9c2ea4e
gustavolitter <gustavolitter@gmail.com>
2023-11-14 11:36:19 -0300

    feat(main):

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,23 +1,4 @@
 package main
 
-import "fmt"
-
 func main() {
-	variavel := 10
-	fmt.Println(variavel)
-	fmt.Println(&variavel)
-
-	ponteiroFalso := variavel
-	fmt.Println(ponteiroFalso)
-
-	ponteiro := &variavel
-	*ponteiro = 20
-	fmt.Println(*ponteiro)
-
-	callback(&variavel)
-	fmt.Println(variavel)
-}
-
-func callback(a *int) {
-	*a = 200
 }

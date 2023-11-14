8a20401a45633de2ac73f089811f474bc7580bbe
gustavolitter <gustavolitter@gmail.com>
2023-11-14 11:35:25 -0300

    feat(main): Criado teste para o usuário.

--- internal/infra/database/user_db_test.go
+++ internal/infra/database/user_db_test.go
@@ -28,3 +28,22 @@ func TestCreateUser(t *testing.T) {
 	assert.Equal(t, user.Email, userFound.Email)
 	assert.NotNil(t, userFound.Password)
 }
+
+func TestFindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entities.User{})
+	user, _ := entities.NewUser("rocket", "rocket@gmail.com", "12345")
+	userDB := NewUser(db)
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, userFound.Password)
+}
